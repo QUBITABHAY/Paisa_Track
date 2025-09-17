c5b643dc47162c07a2554ca623fe7cea01f57752
QUBITABHAY <codingcontestabhay@gmail.com>
2025-09-17 23:07:53 +0530

    Add file structure

---
+++ frontend/App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
+
+export default function App() {
+  return <View className="flex-1 bg-gray-900"></View>;
+}
